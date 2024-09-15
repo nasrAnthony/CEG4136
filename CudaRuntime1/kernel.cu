13e68829feefca2dd715bd4103c22f10464de4ac
SirTonySama <90422210+SirTonySama@users.noreply.github.com>
2024-09-15 16:03:28 -0400

    code

---
+++ CudaRuntime1/kernel.cu
@@ -0,0 +1,17 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__global__ void myFirstKernel() {
+    int blockId = blockIdx.x;
+    int threadId = threadIdx.x;
+    int globalThreadId = blockId * blockDim.x + threadId;
+    printf("Block %d, Thread %d, Global Thread %d\n", blockId, threadId, globalThreadId);
+}
+
+int main() {
+    myFirstKernel << <4, 8 >> > ();
+    cudaDeviceSynchronize();
+    return 0;
+}
+
