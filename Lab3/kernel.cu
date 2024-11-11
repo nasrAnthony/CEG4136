c390a862e68355e6727895ece8bb67adaa96e0e9
SirTonySama <90422210+SirTonySama@users.noreply.github.com>
2024-11-11 15:09:10 -0500

    Changed from int to flat matrices. Added padding

--- Lab3/kernel.cu
+++ Lab3/kernel.cu
@@ -5,16 +5,16 @@
 #include <stdio.h>
 #include <time.h>
 
-//#define T 2 //32 //Tile size 32
-#define T 32
+#define T 2 //32 //Tile size 32
+//#define T 32
 
-void buildMatrix(int* start, int& size) { //helper function to init the matrices before multiplication. 
+void buildMatrix(float* start, int& size) { //helper function to init the matrices before multiplication. 
 	for (int i = 0; i < size; i++) {
-		*(start + i) = rand() % 10;
+		*(start + i) = static_cast<float>(rand() % 10);
 	};
 }
 
-void printMatrix(int* start, int& size) { //helper function to showcase matrix 
+void printMatrix(float* start, int& size) { //helper function to showcase matrix 
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			std::cout << *(start + i * size + j) << " "; //convert 2D index to 1D for printing. 
@@ -24,10 +24,10 @@ void printMatrix(int* start, int& size) { //helper function to showcase matrix
 }
 
 //Kernel implementation
-__global__ void matrixMultKernel(int* A, int* B, int* C, int size) {
+__global__ void matrixMultKernel(float* A, float* B, float* C, int size) {
 	//shared memory setup
-	__shared__ int Ashm[T][T];
-	__shared__ int Bshm[T][T];
+	__shared__ int Ashm[T][T + 1]; //padding to avoid memory bank conflicts
+	__shared__ int Bshm[T][T + 1]; //padding to avoid memory bank conflicts
 	//calculate index
 	int row = blockIdx.y * T + threadIdx.y;
 	int col = blockIdx.x * T + threadIdx.x;
@@ -48,28 +48,28 @@ __global__ void matrixMultKernel(int* A, int* B, int* C, int size) {
 
 int main() {
 
-	//int N = 8;//= 1024;
-	int N = 1024;
+	int N = 8;//= 1024;
+	//int N = 1024;
 	int totalSize = N * N;
 	//fetch total size of array in bytes
-	int size = N * N * sizeof(int);
+	int size = N * N * sizeof(float);
 	//allocate memory on host. 
-	int* hostA = (int*)malloc(size);//cast pointer returned from malloc to type int* || operand A
-	int* hostB = (int*)malloc(size);//cast pointer returned from malloc to type int* || operand B
-	int* hostC = (int*)malloc(size);//cast pointer returned from malloc to type int* || result  C
+	float* hostA = (float*)malloc(size);//cast pointer returned from malloc to type float* || operand A
+	float* hostB = (float*)malloc(size);//cast pointer returned from malloc to type float* || operand B
+	float* hostC = (float*)malloc(size);//cast pointer returned from malloc to type float* || result  C
 
 	//build the matrices with helper function
 	buildMatrix(hostA, totalSize); //init matrix A
 	buildMatrix(hostB, totalSize); //init matrix B
 
 	//Print matrices A and B
-	//std::cout << "Matrix A:" << std::endl;
-	//printMatrix(hostA, N);
-	//std::cout << "\nMatrix B:" << std::endl;
-	//printMatrix(hostB, N);
+	std::cout << "Matrix A:" << std::endl;
+	printMatrix(hostA, N);
+	std::cout << "\nMatrix B:" << std::endl;
+	printMatrix(hostB, N);
 
 	//allocate device memory
-	int* devA; int* devB; int* devC;
+	float* devA; float* devB; float* devC;
 	cudaMalloc((void**)&devA, size);
 	cudaMalloc((void**)&devB, size);
 	cudaMalloc((void**)&devC, size);
@@ -90,8 +90,8 @@ int main() {
 	cudaMemcpy(hostC, devC, size, cudaMemcpyDeviceToHost);
 
 	//Print matrix C
-	//std::cout << "\nResult Matrix C (A * B):" << std::endl;
-	//printMatrix(hostC, N);
+	std::cout << "\nResult Matrix C (A * B):" << std::endl;
+	printMatrix(hostC, N);
 
 	//free up space on device
 	cudaFree(devA); cudaFree(devB); cudaFree(devC);
